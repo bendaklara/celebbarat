278f641eb98058547eb2aa95ea8858cab5b95b6b
Benda Klara <perfectyello@gmail.com>
2018-10-10 12:08:35 +0200

    first commit

--- app.js
+++ app.js
@@ -31,7 +31,70 @@ var pool = mysql.createPool({
   database : config.database,
   connectionLimit: 10
 });
-//Connect to Database only if Config.js parameter is set.
+
+function fbrequest(token, requeststring) {
+	return new Promise(function(resolve, reject) {
+	var success = '0';
+	var generic_error_message='Generic error message'; // Ezt kapja, ha nem azonosítottuk a hiba okát.
+	var errormessage=generic_error_message; 
+	
+	graph
+	.setAccessToken(token)
+	.setOptions(options)
+	.get(requeststring , function(err, fbresponse) {
+		console.log('Raw Fb response: ' + JSON.stringify(fbresponse));
+		
+		// FB error handling
+		if (fbresponse && fbresponse['error']) {
+			// extract the error from the json
+			console.log('Graph api error!!!!');
+			var error=fbresponse['error'];
+			if (error && error['code']) {
+			// extract the error code
+				var code=error['code'];
+				console.log(code);
+				//Let the message be appropriate to the error code
+				switch (code) {
+					case 10:
+						errormessage='Error 10';
+					break;
+
+					case 803:
+						errormessage='Error 803';
+					break;
+
+					case 190:
+						errormessage='Error 109';
+						break;
+
+					default:
+						//Generic error message. 
+						//message='Ooops! There was an error. How about trying another page?';
+						errormessage=generic_error_message ;
+				}
+			
+			} else {
+			errormessage=generic_error_message + ' No code in error message' ;
+			}
+			reject(errormessage);
+		} 
+		//End of FB error handling
+		
+		
+		//Real functionality.
+		else {if (fbresponse) {
+				var message="success";
+				resolve(fbresponse); //This is the meat of the application
+				} else {
+					errormessage='Thrown error'
+					reject(errormessage);
+					
+				}
+			}	
+		});	
+  });
+
+
 
 // Passport session setup.
 passport.serializeUser(function(user, done) {
@@ -53,12 +116,21 @@ passport.use(new FacebookStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
+			fbrequest(accessToken, profile.id +'?fields=id,name,gender,email,birthday,likes{id}').then(function(response) {
+				console.log('Visszakaptam a fbrequest-tol a response-t');
+				console.log(response);
+				}, function(error) {
+				console.log('Visszakaptam a fbrequest error response-t');		
+				console.log(response);
+			}
+		
 			var user = {
 				'id'   : profile.id,
 				'displayName'   : profile.displayName,				
 				'token': accessToken
 			}
-		
+
+			
 			pool.getConnection().then(function(connection){
 				connection.query("SELECT * from Fb_User where fb_id="+profile.id,function(err,rows,fields){
 				if(err) throw err;
@@ -78,7 +150,7 @@ passport.use(new FacebookStrategy({
 		  }).catch(function(err) {
 			console.log(err);
 		});		  
-	  console.log(profile);
+	  //console.log(profile);
       return done(null, user);
     });
   }
