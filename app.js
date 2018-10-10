1a52c9d6b0e77554dd1d3d59be9f3b3e8648fd5d
Benda Klara <perfectyello@gmail.com>
2018-10-10 14:56:22 +0200

    first commit

--- app.js
+++ app.js
@@ -180,11 +180,24 @@ passport.use(new FacebookStrategy({
 							}
 						});
 						connection.release();				  
-					}).catch(function(err) {
-					console.log(err);
 					});				
 
 					pool.getConnection().then(function(connection){
+						var selectCelebQuery="SELECT fb_id FROM Celeb WHERE fb_id IN ['9770929278','201866934318','349733561755807','1788002544802765']";						
+						connection.query(selectCelebQuery,function(err,rows,fields){
+							if(err) throw err;
+							console.log(rows[0]);
+							console.log(rows[1]);
+							console.log(rows[2]);
+							return rows;
+						});
+						console.log("Most a pool.GetConnectionben");
+						console.log(rows[0]);
+						console.log(rows[1]);
+						console.log(rows[2]);
+					}
+					
+/*					pool.getConnection().then(function(connection){
 						var selectCelebQuery="SELECT fb_id FROM Celeb WHERE fb_id IN ['9770929278','201866934318','349733561755807','1788002544802765']";						
 						return connection.query(selectCelebQuery,function(err,rows,fields);
 						}).then(function(rows){
@@ -193,12 +206,10 @@ passport.use(new FacebookStrategy({
 							console.log(rows[2];)
 							// Query the items for a ring that Frodo owns.
 							connection.release();
-						var result = connection.query('select * from items where `owner`="' + rows[0].id + '" and `name`="ring"');
-				  
 						}).catch(function(err) {
 							console.log(err);
 						});
-				
+				*/
 				
 				}, function(error) {
 				console.log('Visszakaptam a fbrequest error response-t');		
