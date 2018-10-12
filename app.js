db5facb462ec95a0ecedf7516ddf0c00321f13ed
Benda Klara <perfectyello@gmail.com>
2018-10-12 15:30:53 +0200

    first commit

--- app.js
+++ app.js
@@ -32,75 +32,6 @@ var pool = mysql.createPool({
   connectionLimit: 10
 });
 
-function fbcelebrequest(token, requeststring) {
-	return new Promise(function(resolve, reject) {
-	var success = '0';
-	var generic_error_message='Generic error message'; // Ezt kapja, ha nem azonosítottuk a hiba okát.
-	var errormessage=generic_error_message; 
-	
-	graph
-	.setAccessToken(token)
-	.setOptions(options)
-	.get(requeststring , function(err, fbresponse) {
-		console.log('Raw Fb response: ' + JSON.stringify(fbresponse));
-		// FB error handling
-		if (fbresponse && fbresponse['error']) {
-			// extract the error from the json
-			console.log('Graph api error!!!!');
-			var error=fbresponse['error'];
-			if (error && error['code']) {
-			// extract the error code
-				var code=error['code'];
-				console.log(code);
-				//Let the message be appropriate to the error code
-				switch (code) {
-					case 10:
-						errormessage='Error 10';
-					break;
-
-					case 803:
-						errormessage='Error 803';
-					break;
-
-					case 190:
-						errormessage='Error 109';
-						break;
-
-					default:
-						//Generic error message. 
-						//message='Ooops! There was an error. How about trying another page?';
-						errormessage=generic_error_message ;
-				}
-			
-			} else {
-			errormessage=generic_error_message + ' No code in error message' ;
-			}
-			reject(errormessage);
-		} 
-		//End of FB error handling
-		//Real functionality.
-		else if (fbresponse) {
-				console.log("FB response: " + fbresponse);
-				var picUrl;
-				if(fbresponse.data){
-					picUrl=fbresponse.data.url;
-					resolve(picUrl); //This is the meat of the application
-				} 
-				else if(fbresponse.image){
-					picUrl=fbresponse.location;
-					console.log("Pic URL: " + picUrl);
-					resolve(picUrl); //This is the meat of the application
-				} 				
-				else {
-					errormessage='Thrown error'
-					reject(errormessage);
-				}
-			}
-		
-	});	
-  });
-}
-
 function fbrequest(user, token, requeststring) {
 	return new Promise(function(resolve, reject) {
 	var success = '0';
@@ -360,7 +291,7 @@ function mysqlrequest(user, connection) {
 					user.celeb_name=rows[0].celeb_name;
 					user.fbLink=facebookLink;
 					console.log("Req User updated");
-					var graphurl=rows[0].celeb_fb_id +"/picture";
+/*					var graphurl=rows[0].celeb_fb_id +"/picture";
 					console.log("Celeb pic graph url: " + graphurl)
 					console.log("User token" + user.token);
 					fbcelebrequest(user.token, graphurl)
@@ -374,7 +305,7 @@ function mysqlrequest(user, connection) {
 							reject(user);
 						}
 					);					
-														
+*/														
 				} else {
 					setTimeout(
 					  () => {
@@ -471,3 +402,75 @@ function ensureAuthenticated(req, res, next) {
 app.listen(5006, () => {
   console.log('Server is up on port 5006');
 });
+
+
+/*
+function fbcelebrequest(token, requeststring) {
+	return new Promise(function(resolve, reject) {
+	var success = '0';
+	var generic_error_message='Generic error message'; // Ezt kapja, ha nem azonosítottuk a hiba okát.
+	var errormessage=generic_error_message; 
+	
+	graph
+	.setAccessToken(token)
+	.setOptions(options)
+	.get(requeststring , function(err, fbresponse) {
+		console.log('Raw Fb response: ' + JSON.stringify(fbresponse));
+		// FB error handling
+		if (fbresponse && fbresponse['error']) {
+			// extract the error from the json
+			console.log('Graph api error!!!!');
+			var error=fbresponse['error'];
+			if (error && error['code']) {
+			// extract the error code
+				var code=error['code'];
+				console.log(code);
+				//Let the message be appropriate to the error code
+				switch (code) {
+					case 10:
+						errormessage='Error 10';
+					break;
+
+					case 803:
+						errormessage='Error 803';
+					break;
+
+					case 190:
+						errormessage='Error 109';
+						break;
+
+					default:
+						//Generic error message. 
+						//message='Ooops! There was an error. How about trying another page?';
+						errormessage=generic_error_message ;
+				}
+			
+			} else {
+			errormessage=generic_error_message + ' No code in error message' ;
+			}
+			reject(errormessage);
+		} 
+		//End of FB error handling
+		//Real functionality.
+		else if (fbresponse) {
+				console.log("FB response: " + fbresponse);
+				var picUrl;
+				if(fbresponse.data){
+					picUrl=fbresponse.data.url;
+					resolve(picUrl); //This is the meat of the application
+				} 
+				else if(fbresponse.image){
+					picUrl=fbresponse.location;
+					console.log("Pic URL: " + picUrl);
+					resolve(picUrl); //This is the meat of the application
+				} 				
+				else {
+					errormessage='Thrown error'
+					reject(errormessage);
+				}
+			}
+		
+	});	
+  });
+}
+*/
